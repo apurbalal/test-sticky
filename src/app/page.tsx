8ec06ba565c9d52862582097bc9d9bcec6674c4f
Apurbalal Singha <apurbalal@mercari.com>
2024-03-07 15:18:10 +0900

    fix

---
+++ src/app/page.tsx
@@ -0,0 +1,109 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className="Container">
+      <div className="HeaderWrapper">
+        Header
+      </div>
+      <div className="ListWrapper">
+        <ul>
+          <li>
+            <div className="ListItem">1</div>
+          </li>
+          <li>
+            <div className="ListItem">2</div>
+          </li>
+          <li>
+            <div className="ListItem">3</div>
+          </li>
+          <li>
+            <div className="ListItem">4</div>
+          </li>
+          <li>
+            <div className="ListItem">5</div>
+          </li>
+          <li>
+            <div className="ListItem">6</div>
+          </li>
+          <li>
+            <div className="ListItem">7</div>
+          </li>
+          <li>
+            <div className="ListItem">8</div>
+          </li>
+          <li>
+            <div className="ListItem">9</div>
+          </li>
+          <li>
+            <div className="ListItem">10</div>
+          </li>
+          <li>
+            <div className="ListItem">11</div>
+          </li>
+          <li>
+            <div className="ListItem">12</div>
+          </li>
+          <li>
+            <div className="ListItem">13</div>
+          </li>
+          <li>
+            <div className="ListItem">14</div>
+          </li>
+          <li>
+            <div className="ListItem">15</div>
+          </li>
+          <li>
+            <div className="ListItem">16</div>
+          </li>
+          <li>
+            <div className="ListItem">17</div>
+          </li>
+          <li>
+            <div className="ListItem">18</div>
+          </li>
+          <li>
+            <div className="ListItem">19</div>
+          </li>
+          <li>
+            <div className="ListItem">20</div>
+          </li>
+          <li>
+            <div className="ListItem">21</div>
+          </li>
+          <li>
+            <div className="ListItem">22</div>
+          </li>
+          <li>
+            <div className="ListItem">23</div>
+          </li>
+          <li>
+            <div className="ListItem">24</div>
+          </li>
+          <li>
+            <div className="ListItem">25</div>
+          </li>
+          <li>
+            <div className="ListItem">26</div>
+          </li>
+          <li>
+            <div className="ListItem">27</div>
+          </li>
+          <li>
+            <div className="ListItem">28</div>
+          </li>
+          <li>
+            <div className="ListItem">29</div>
+          </li>
+          <li>
+            <div className="ListItem">30</div>
+          </li>
+        </ul>
+      </div>
+      <div className="InputWrapper">
+        <input type="text" placeholder="Enter text" />
+      </div>
+    </div>
+  );
+}
